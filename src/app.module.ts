8622870d7ebf2311d80c48a8aea34f723e026ccf
Rômulo Valentim <valentim.canejo@hotmail.com>
2025-03-31 14:57:46 -0300

    feat: cria módulo de exames

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ExamesController } from './exame/exame.controller';
 import { PrismaDBModule } from './prisma-db/db.module';
+import { ExamesModule } from './exame/exame.module';
 
 @Module({
-  imports: [ExamesController, PrismaDBModule],
+  imports: [ExamesModule, PrismaDBModule],
   controllers: [AppController],
   providers: [AppService],
 })
